4f46ba4127c319f66b5a836273a5d79c63e157d6
Petter Taule <petter.taule@tum.de>
2019-03-20 16:45:21 +0100

    Evaluate integrand for fixed Q1,Q2-vectors

--- main.c
+++ main.c
@@ -10,8 +10,6 @@
 
 #include <gsl/gsl_sf.h>
 
-#include <cuba.h>
-
 #include "include/constants.h"
 #include "include/utilities.h"
 #include "include/kernels.h"
@@ -29,64 +27,8 @@
 // Input power spectrum file
 #define INPUT_FILE "input/PS_linear_z000_pk.dat"
 // Output power spectrum to file
-#define OUTPUT_FILE "output_" TOSTRING(LOOPS) "loop.dat"
-
-// CUBA settings
-#define NVEC 1
-#define EPSREL 1e-3
-#define EPSABS 1e-12
-#define VERBOSE 0
-#define LAST 4
-#define SEED 0
-#define MINEVAL 0
-#define MAXEVAL 1e6
-
-#define STATEFILE NULL
-#define SPIN NULL
-
-#define NNEW 1000
-#define NMIN 2
-#define FLATNESS 25.
-
-
-int cuba_integrand(
-        const int *ndim,
-        const cubareal xx[],
-        const int *ncomp,
-        cubareal ff[],
-        void *userdata
-        )
-{
-    integration_input_t* data = (integration_input_t*)userdata;
-    integration_variables_t vars;
-
-    vfloat ratio = K_MAX/K_MIN;
-
-    vfloat jacobian = 0.0;
-    switch (LOOPS) {
-        case 1:
-            vars.magnitudes[0] = K_MIN * pow(ratio,xx[0]);
-            vars.cos_theta[0] = xx[1];
-            jacobian = log(ratio) * pow(vars.magnitudes[0],3);
-            break;
-        case 2:
-            vars.magnitudes[0] = K_MIN * pow(ratio,xx[0]);
-            vars.magnitudes[1] = K_MIN * pow(ratio,xx[0] * xx[1]);
-            vars.cos_theta[0] = xx[2];
-            vars.cos_theta[1] = xx[3];
-            vars.phi[0] = xx[4] * TWOPI;
-            jacobian = TWOPI * xx[0]
-                * pow(log(ratio),2)
-                * pow(vars.magnitudes[0],3)
-                * pow(vars.magnitudes[1],3);
-            break;
-        default:
-            warning_verbose("No jacobian for LOOPS = %d",LOOPS);
-    }
-
-    ff[0] = jacobian * integrand(data,&vars);
-    return 0;
-}
+#define OUTPUT_FILE \
+    "output_" TOSTRING(LOOPS) "loop_fixed_momenta.dat"
 
 
 
@@ -111,19 +53,14 @@ int main () {
         .spline = spline
     };
 
+    integration_variables_t vars = {
+        .magnitudes = {0.3,0.2},
+        .cos_theta  = {1,1},
+        .phi        = {0}
+    };
+
     double* wavenumbers    = (double*)calloc(N_POINTS, sizeof(double));
     double* power_spectrum = (double*)calloc(N_POINTS, sizeof(double));
-
-    // Overall factors:
-    // - Only integrating over cos_theta_i between 0 and 1, multiply by 2 to
-    //   obtain [-1,1]
-    // - Assuming Q1 > Q2 > ..., hence multiply result by LOOPS factorial
-    // - Phi integration of first loop momenta gives a factor 2pi
-    // - Conventionally divide by (2pi)^3
-    vfloat overall_factor = pow(2,LOOPS) * gsl_sf_fact(LOOPS) * TWOPI;
-
-    int nregions, neval, fail;
-    cubareal result[1], error[1], prob[1];
 
     double delta_logk = log(K_MAX/K_MIN) / N_POINTS;
     double k = K_MIN;
@@ -133,19 +70,9 @@ int main () {
         wavenumbers[i] = k;
         data.k = k;
 
-        Suave(N_DIMS, 1, cuba_integrand, &data,
-                NVEC, EPSREL, EPSABS, VERBOSE | LAST, SEED,
-                MINEVAL, MAXEVAL, NNEW, NMIN, FLATNESS,
-                STATEFILE, SPIN,
-                &nregions, &neval, &fail, result, error, prob);
+        power_spectrum[i] = integrand(&data,&vars) / gsl_spline_eval(data.spline,k,data.acc);
 
-        result[0] *= overall_factor;
-        error[0] *= overall_factor;
-
-        power_spectrum[i] = (double)result[0];
-
-        printf("k  = %f, result = %e, error = %f, prob = %f\n",
-                k, (double)*result, (double)error[0], (double)prob[0]);
+        printf("k  = %f, result = %e,\n", k, power_spectrum[i]);
     }
 
     write_PS(OUTPUT_FILE,N_POINTS,wavenumbers,power_spectrum);
