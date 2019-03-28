fa45157c3321914e22240be9663920d8900e9aca
Petter Taule <petter.taule@tum.de>
2019-03-28 17:18:37 +0100

    Include stdlib.h for malloc and friends

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #include <gsl/gsl_sf.h>
--- src/integrand.c
+++ src/integrand.c
@@ -5,6 +5,7 @@
    Copyright (c) 2019 Petter Taule. All rights reserved.
 */
 
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
