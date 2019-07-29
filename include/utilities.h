31ecee8719e11ac318fbf83f0cba56ef166e8ac5
Petter Taule <petter.taule@tum.de>
2019-07-29 16:01:58 +0200

    Use matrix_t for print_gsl_matrix function

--- include/utilities.h
+++ include/utilities.h
@@ -58,7 +58,7 @@ void initialize_timesteps(double eta[], double eta_i, double eta_f);
 
 void print_label(short int label);
 void print_labels(const short int labels[]);
-void print_gsl_matrix(const gsl_matrix* m, size_t height, size_t width);
+void print_gsl_matrix(const matrix_t* m, size_t height, size_t width);
 
 short int zero_label();
 bool is_fundamental(short int label);
