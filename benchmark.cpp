aa46545002bcd86c9a31f018c7394ce53b0690c2
Petter Taule <pettertaule@live.no>
2020-11-07 13:51:18 +0100

    Add EdS-SPT bispectrum to benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -40,13 +40,13 @@ static void BM_ps_integrand(benchmark::State& state) {
     // Perform setup here
     int n_loops = 2;
 
-    double k1 = 0.2;
+    double k_a = 0.2;
     double q_min = 1e-4;
     double q_max = 65;
 
     Interpolation1D input_ps("/home/pettertaule/repos/class_public/output/fiducial/newtonian/z1_pk.dat");
 
-    Vec1D<Pair<int>> pair_correlations = {{0,0}};
+    Vec1D<Pair<int>> correlations = {{0,0}};
 
     LoopParameters loop_params(n_loops, POWERSPECTRUM, EDS_SPT);
     SumTable sum_table(loop_params);
@@ -54,15 +54,15 @@ static void BM_ps_integrand(benchmark::State& state) {
     EtaGrid eta_grid;
 
     Vec1D<IntegrandTables> tables_vec;
-    tables_vec.emplace_back(k1, loop_params, sum_table, ev_params, eta_grid);
+    tables_vec.emplace_back(k_a, loop_params, sum_table, ev_params, eta_grid);
 
     Vec1D<PowerSpectrumDiagram> diagrams = ps::construct_diagrams(loop_params);
 
-    IntegrationInput input(q_min, q_max, &diagrams, &pair_correlations, input_ps,
+    IntegrationInput input(q_min, q_max, &diagrams, &correlations, input_ps,
             tables_vec);
 
     int ndim = 3 * n_loops - 1;
-    int ncomp = pair_correlations.size();
+    int ncomp = correlations.size();
 
     double* xx = new double[ndim];
     double* ff = new double[ncomp];
@@ -84,8 +84,60 @@ static void BM_ps_integrand(benchmark::State& state) {
 }
 
 
+static void BM_bs_integrand(benchmark::State& state) {
+    // Perform setup here
+    int n_loops = 1;
+
+    double k_a = 0.1;
+    double k_b = 0.2;
+    double cos_ab = 0.5;
+    double q_min = 1e-4;
+    double q_max = 65;
+
+    Interpolation1D input_ps("/home/pettertaule/repos/class_public/output/fiducial/newtonian/z1_pk.dat");
+
+    Vec1D<Triple<int>> correlations = {{0,0,0}};
+
+    LoopParameters loop_params(n_loops, BISPECTRUM, EDS_SPT);
+    SumTable sum_table(loop_params);
+    EvolutionParameters ev_params;
+    EtaGrid eta_grid;
+
+    Vec1D<IntegrandTables> tables_vec;
+    tables_vec.emplace_back(k_a, k_b, cos_ab, loop_params, sum_table, ev_params, eta_grid);
+
+    Vec1D<BiSpectrumDiagram> diagrams = bs::construct_diagrams(loop_params);
+
+    IntegrationInput input(q_min, q_max, &diagrams, &correlations, input_ps,
+            tables_vec);
+
+    int ndim = 3 * n_loops;
+    int ncomp = correlations.size();
+
+    double* xx = new double[ndim];
+    double* ff = new double[ncomp];
+
+    for (int i = 0; i < ndim; ++i) {
+        xx[i] = 0.5;
+    }
+
+    for (auto _ : state) {
+        // This code gets timed
+        int nvec = 1;
+        /* core = -1 in accordance with bs::integrand() or bs::integrand() */
+        int core = -1;
+        bs::integrand(&ndim, xx, &ncomp, ff, &input, &nvec, &core);
+    }
+
+    delete[] xx;
+    delete[] ff;
+}
+
+
+
 // Register the functions as benchmarks
 /* BENCHMARK(BM_kernel_index); */
 BENCHMARK(BM_ps_integrand);
+BENCHMARK(BM_bs_integrand);
 // Run the benchmark
 BENCHMARK_MAIN();
