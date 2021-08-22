3af8cd38f70149dde38bf513912d711fba6fe3aa
Petter Taule <petter.taule@tum.de>
2021-08-22 11:24:28 +0200

    Fix bug in build/submit scripts from e2b3e0f

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,15 +3,15 @@ set -e
 set -x
 
 cleanup() {
-    rm -rf $tempdir
+    rm -rf "$tempdir"
 }
 
 # Run cleanup on EXIT
 trap cleanup EXIT
 
 # Set some default values:
-N_CORES=4
-LOG_FILE=unset
+n_cores=4
+log_file=
 
 usage()
 {
@@ -30,7 +30,7 @@ while :
 do
   case "$1" in
     -h | --help) usage ; shift 2 ;;
-    -p | --n_cores) N_CORES=$2 ; shift 2 ;;
+    -p | --n_cores) n_cores=$2 ; shift 2 ;;
     # -- means the end of the arguments; drop this, and break out of the while loop
     --) shift; break ;;
     # If invalid options were passed, then getopt should have reported an error,
@@ -44,7 +44,7 @@ if [ $# -ne 1 ]; then
   usage
 fi
 
-LOG_FILE=$1
+log_file=$1
 
 julia=/space/ge52sir/local/bin/julia
 isapprox=/home/t30/all/ge52sir/non_linear_PS/tests/isapprox.jl
@@ -56,85 +56,88 @@ LIBCONFIG="libconfig-1.7.2"
 LIBPATH="/space/ge52sir/"
 
 sourcedir=$(pwd)
-tempdir="$(mktemp -d)"
+tempdir="$(mktemp -d -t cosPT_XXXXXXXXXX)"
 
-mkdir -p $tempdir/local
-mkdir -p $tempdir/output
-mkdir -p $tempdir/ini
-mkdir -p $tempdir/tests
+mkdir -p "$tempdir"/local
+mkdir -p "$tempdir"/output
+mkdir -p "$tempdir"/ini
+mkdir -p "$tempdir"/tests
 
-cp -r $LIBPATH/$GSL $tempdir
-cp -r $LIBPATH/$CUBA $tempdir
-cp -r $LIBPATH/$LIBCONFIG $tempdir
-cp -r -t $tempdir $sourcedir/src $sourcedir/include \
-    $sourcedir/main.cpp $sourcedir/Makefile
-cp -r -t $tempdir/ini $sourcedir/ini/tests/*
-cp -r -t $tempdir/tests $sourcedir/tests/data
+cp -r $LIBPATH/$GSL "$tempdir"
+cp -r $LIBPATH/$CUBA "$tempdir"
+cp -r $LIBPATH/$LIBCONFIG "$tempdir"
+cp -r -t "$tempdir" "$sourcedir"/src "$sourcedir"/include \
+    "$sourcedir"/main.cpp "$sourcedir"/Makefile
+cp -r -t "$tempdir"/ini "$sourcedir"/ini/tests/*
+cp -r -t "$tempdir"/tests "$sourcedir"/tests/data
 
-cd $tempdir/$LIBCONFIG
+cd "$tempdir"/"$LIBCONFIG"
 autoreconf -f -i
-./configure --prefix=$tempdir/local/
+./configure --prefix="$tempdir"/local/
 make clean
 make -j && make -j install
 
-cd $tempdir/$CUBA
-./configure --prefix=$tempdir/local/
+cd "$tempdir/$CUBA"
+./configure --prefix="$tempdir"/local/
 make clean
 make -j install
 
-cd $tempdir/$GSL
-./configure --prefix=$tempdir/local/
+cd "$tempdir/$GSL"
+./configure --prefix="$tempdir"/local/
 make clean
 make -j && make -j install
 
-cd $sourcedir
+# Remember git sha from source directory
+cd "$sourcedir"
 git_sha=$(git rev-parse HEAD)
-cd $tempdir
-printf "#include \"../include/version.hpp\"\\nstd::string build_git_sha =  \"${git_sha}\";" \
-    > src/version_.cpp
 
-cd $tempdir
-mkdir obj
-mkdir build
+cd "$tempdir"
+# Create src/version_.cpp with git sha from source directory, and add this file
+# to the source files listed in the makefile
+printf "#include \"../include/version.hpp\"\nstd::string build_git_sha =  \"%s\";" \
+    $git_sha > src/version_.cpp
+sed -i '/SRC_FILES/ s/\\$/version_.cpp \\/' Makefile
 make clean
 make -j cluster
-export LD_LIBRARY_PATH=$tempdir/local/lib/
+export LD_LIBRARY_PATH="$tempdir"/local/lib/
 
-mkdir -p $tempdir/output/eds_spt_ps/L1
-mkdir -p $tempdir/output/eds_spt_ps/L2
-mkdir -p $tempdir/output/eds_spt_bs/L1
-mkdir -p $tempdir/output/eds_spt_bs/L2
+mkdir -p "$tempdir"/output/eds_spt_ps/L1
+mkdir -p "$tempdir"/output/eds_spt_ps/L2
+mkdir -p "$tempdir"/output/eds_spt_bs/L1
+mkdir -p "$tempdir"/output/eds_spt_bs/L2
 
-echo "Integration test" > $LOG_FILE
-echo "$(date)" >> $LOG_FILE
+{
+    echo "Integration test"
+    date
+} > "$log_file"
 
-for K_A in {000,005,010,015,020,025,030,035,040,045,050,055}; do
-    $tempdir/$exe --k_a $K_A --n_cores $N_CORES $tempdir/ini/eds_spt_ps_L1.cfg \
-        >> $LOG_FILE
-    $tempdir/$exe --k_a $K_A --n_cores $N_CORES $tempdir/ini/eds_spt_ps_L2.cfg \
-        >> $LOG_FILE
+for k_a in {000,005,010,015,020,025,030,035,040,045,050,055}; do
+    {
+        "$tempdir"/"$exe" --k_a_idx $k_a --n_cores $n_cores "$tempdir"/ini/eds_spt_ps_L1.cfg
+        "$tempdir"/"$exe" --k_a_idx $k_a --n_cores $n_cores "$tempdir"/ini/eds_spt_ps_L2.cfg
+    } >> "$log_file"
 done
 
-for K_A in {000,010,020,030,040,050,060,070,080,090,100}; do
-    $tempdir/$exe --k_a_idx $K_A --k_b_idx $K_A --k_c_idx $K_A --n_cores $N_CORES \
-        $tempdir/ini/eds_spt_bs_L1.cfg >> $LOG_FILE
-    $tempdir/$exe --k_a_idx $K_A --k_b_idx $K_A --k_c_idx $K_A --n_cores $N_CORES \
-        $tempdir/ini/eds_spt_bs_L2.cfg >> $LOG_FILE
+for k_a in {000,010,020,030,040,050,060,070,080,090,100}; do
+    {
+        "$tempdir"/"$exe" --k_a_idx $k_a --k_b_idx $k_a --k_c_idx $k_a --n_cores $n_cores \
+            "$tempdir"/ini/eds_spt_bs_L1.cfg
+        "$tempdir"/"$exe" --k_a_idx $k_a --k_b_idx $k_a --k_c_idx $k_a --n_cores $n_cores \
+            "$tempdir"/ini/eds_spt_bs_L2.cfg
+    } >> "$log_file"
 done
 
 for f in output/*/*/; do
-    cat $f/* > $f/total.dat
+    cat "$f"/* > "$f"/total.dat
 done
 
-$julia $isapprox --col_A 3 --col_err_A 4 --col_B 3 --col_err_B 4 \
-    $tempdir/tests/data/eds_spt_ps/L1/total.dat $tempdir/output/eds_spt_ps/L1/total.dat \
-    >> $LOG_FILE
-$julia $isapprox --col_A 3 --col_err_A 4 --col_B 3 --col_err_B 4 \
-    $tempdir/tests/data/eds_spt_ps/L2/total.dat $tempdir/output/eds_spt_ps/L2/total.dat \
-    >> $LOG_FILE
-$julia $isapprox --col_A 5 --col_err_A 6 --col_B 5 --col_err_B 6 \
-    $tempdir/tests/data/eds_spt_bs/L1/total.dat $tempdir/output/eds_spt_bs/L1/total.dat \
-    >> $LOG_FILE
-$julia $isapprox --col_A 5 --col_err_A 6 --col_B 5 --col_err_B 6 \
-    $tempdir/tests/data/eds_spt_bs/L2/total.dat $tempdir/output/eds_spt_bs/L2/total.dat \
-    >> $LOG_FILE
+{
+    "$julia" "$isapprox" --col_A 3 --col_err_A 4 --col_B 3 --col_err_B 4 \
+        "$tempdir"/tests/data/eds_spt_ps/L1/total.dat "$tempdir"/output/eds_spt_ps/L1/total.dat
+    "$julia" "$isapprox" --col_A 3 --col_err_A 4 --col_B 3 --col_err_B 4 \
+        "$tempdir"/tests/data/eds_spt_ps/L2/total.dat "$tempdir"/output/eds_spt_ps/L2/total.dat
+    "$julia" "$isapprox" --col_A 5 --col_err_A 6 --col_B 5 --col_err_B 6 \
+        "$tempdir"/tests/data/eds_spt_bs/L1/total.dat "$tempdir"/output/eds_spt_bs/L1/total.dat
+    "$julia" "$isapprox" --col_A 5 --col_err_A 6 --col_B 5 --col_err_B 6 \
+        "$tempdir"/tests/data/eds_spt_bs/L2/total.dat "$tempdir"/output/eds_spt_bs/L2/total.dat
+} >> "$log_file"
--- tests/submit.sh
+++ tests/submit.sh
@@ -3,5 +3,5 @@
 julia_depot_path=/space/ge52sir/.julia
 log_dir="/space/ge52sir/sge_output"
 
-qsub -N $1 -pe smp 4 -cwd -e "${log_dir}/error/" -o "${log_dir}/output/" \
+qsub -N $1 -pe smp 4 -cwd -e "${log_dir}"/error/ -o "${log_dir}"/output/ \
     -v JULIA_DEPOT_PATH=$julia_depot_path tests/run_tests.sh $2
